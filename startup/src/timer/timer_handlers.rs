ae5a5999959d9b55b76ba85d87aebe23ceadf397
Olivier Lemoine <olivier@le-moine.fr>
2019-04-12 07:00:11 +0200

    Add reset irq state

--- startup/src/timer/timer_handlers.rs
+++ startup/src/timer/timer_handlers.rs
@@ -2,5 +2,5 @@ use super::super::register::{Bit, Register};
 
 #[no_mangle]
 pub unsafe extern "C" fn TIM2_IRQHandler() {
-    Bit::new(Register::new(0x4000_0010), 6).set(false);
+    Bit::new(Register::new(0x4000_0010), 0).set(false);
 }
