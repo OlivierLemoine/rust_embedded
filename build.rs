9451ab28c5b7706861eea71e0b859410db0a21bd
olivierlemoine <olivier.lemoine@sii.fr>
2019-04-12 11:41:54 +0200

    Add cargo build (Yes, finnaly working !)

--- build.rs
+++ build.rs
@@ -1,18 +1,20 @@
-extern crate cc;
+use std::process::Command;
 
 fn main() {
-    cc::Build::new().
-        .file("./boot/vector_table.S")
-        .flag("-c")
-        .flag("-O0")
-        .flag("-mcpu=cortex-m4")
-        .flag("-mthumb")
-        .compile("vector-table");
-    cc::Build::new()
-        .file("./boot/core.S")
-        .flag("-c")
-        .flag("-O0")
-        .flag("-mcpu=cortex-m4")
-        .flag("-mthumb")
-        .compile("core");
+    Command::new("arm-none-eabi-gcc")
+        .args(&["./boot/vector_table.S",
+            "-C",
+            "-O0",
+            "-mcpu=cortex-m4",
+            "-mthumb",
+            "-o ./boot/vector_table.o"]
+        );
+    Command::new("arm-none-eabi-gcc")
+        .args(&["./boot/core.S",
+            "-C",
+            "-O0",
+            "-mcpu=cortex-m4",
+            "-mthumb",
+            "-o ./boot/core.o"]
+        );
 }
