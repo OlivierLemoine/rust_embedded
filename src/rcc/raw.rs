f50205952cd6d68f5d046044b32935bb5b843294
Olivier Lemoine <olivier@le-moine.fr>
2019-04-22 23:18:56 +0200

    Add som rcc register

--- src/rcc/raw.rs
+++ src/rcc/raw.rs
@@ -2,16 +2,6 @@
 
 use super::super::register::{Bit, Register};
 
-// mod timer_handlers;
-
-// pub type TimerAddr = u32;
-// pub const TIMER_2: TimerAddr = 0x4000_0000;
-// pub const TIMER_3: TimerAddr = 0x4000_0400;
-// pub const TIMER_4: TimerAddr = 0x4000_0800;
-// pub const TIMER_5: TimerAddr = 0x4000_0C00;
-// pub const TIMER_6: TimerAddr = 0x4000_1000;
-// pub const TIMER_7: TimerAddr = 0x4000_1400;
-
 pub struct Rcc {
     base: u32,
 }
@@ -19,6 +9,30 @@ pub struct Rcc {
 impl Rcc {
     pub fn new() -> Rcc {
         Rcc { base: 0x4002_3800 }
+    }
+
+    pub fn pll_sai_is_ready(&self) -> Bit {
+        Bit::new(Register::new(self.base), 25)
+    }
+
+    pub fn pll_sai_on_not_off(&self) -> Bit {
+        Bit::new(Register::new(self.base), 24)
+    }
+
+    pub fn pll_i2s_is_ready(&self) -> Bit {
+        Bit::new(Register::new(self.base), 25)
+    }
+
+    pub fn pll_i2s_on_not_off(&self) -> Bit {
+        Bit::new(Register::new(self.base), 24)
+    }
+
+    pub fn main_pll_is_ready(&self) -> Bit {
+        Bit::new(Register::new(self.base), 25)
+    }
+
+    pub fn main_pll_on_not_off(&self) -> Bit {
+        Bit::new(Register::new(self.base), 24)
     }
 
     pub fn hse_is_ready(&self) -> Bit {
@@ -37,6 +51,60 @@ impl Rcc {
         Bit::new(Register::new(self.base), 0)
     }
 
+    pub fn pll_r(&self) -> (Bit, Bit, Bit) {
+        (
+            Bit::new(Register::new(self.base + 0x04), 30),
+            Bit::new(Register::new(self.base + 0x04), 29),
+            Bit::new(Register::new(self.base + 0x04), 28),
+        )
+    }
+
+    pub fn pll_q(&self) -> (Bit, Bit, Bit, Bit) {
+        (
+            Bit::new(Register::new(self.base + 0x04), 27),
+            Bit::new(Register::new(self.base + 0x04), 26),
+            Bit::new(Register::new(self.base + 0x04), 25),
+            Bit::new(Register::new(self.base + 0x04), 24),
+        )
+    }
+
+    //i2s pll = audio pll
+    pub fn main_pll_and_i2s_pll_src(&self) -> Bit {
+        Bit::new(Register::new(self.base + 0x04), 22)
+    }
+
+    pub fn pll_p(&self) -> (Bit, Bit) {
+        (
+            Bit::new(Register::new(self.base + 0x04), 17),
+            Bit::new(Register::new(self.base + 0x04), 16),
+        )
+    }
+
+    pub fn pll_n(&self) -> (Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit, Bit) {
+        (
+            Bit::new(Register::new(self.base + 0x04), 14),
+            Bit::new(Register::new(self.base + 0x04), 13),
+            Bit::new(Register::new(self.base + 0x04), 12),
+            Bit::new(Register::new(self.base + 0x04), 11),
+            Bit::new(Register::new(self.base + 0x04), 10),
+            Bit::new(Register::new(self.base + 0x04), 9),
+            Bit::new(Register::new(self.base + 0x04), 8),
+            Bit::new(Register::new(self.base + 0x04), 7),
+            Bit::new(Register::new(self.base + 0x04), 6),
+        )
+    }
+
+    pub fn pll_m(&self) -> (Bit, Bit, Bit, Bit, Bit, Bit) {
+        (
+            Bit::new(Register::new(self.base + 0x04), 5),
+            Bit::new(Register::new(self.base + 0x04), 4),
+            Bit::new(Register::new(self.base + 0x04), 3),
+            Bit::new(Register::new(self.base + 0x04), 2),
+            Bit::new(Register::new(self.base + 0x04), 1),
+            Bit::new(Register::new(self.base + 0x04), 0),
+        )
+    }
+
     pub fn system_clock_status(&self) -> (Bit, Bit) {
         (
             Bit::new(Register::new(self.base + 0x08), 3),
@@ -51,59 +119,3 @@ impl Rcc {
         )
     }
 }
-
-//     pub fn auto_reload_register_enabled(&self) -> Bit {
-//         Bit::new(Register::new(self.base), 7)
-//     }
-
-//     pub fn count_direction(&self) -> Bit {
-//         Bit::new(Register::new(self.base), 4)
-//     }
-
-//     pub fn one_pulse_mode(&self) -> Bit {
-//         Bit::new(Register::new(self.base), 3)
-//     }
-
-//     pub fn update_disabled(&self) -> Bit {
-//         Bit::new(Register::new(self.base), 1)
-//     }
-
-//     pub fn count(&self) -> Bit {
-//         Bit::new(Register::new(self.base), 0)
-//     }
-
-//     pub fn clock_division(&self) -> (Bit, Bit) {
-//         (
-//             Bit::new(Register::new(self.base), 9),
-//             Bit::new(Register::new(self.base), 8),
-//         )
-//     }
-
-//     pub fn trigger_interrupt_enabled(&self) -> Bit {
-//         Bit::new(Register::new(self.base + 0x0C), 6)
-//     }
-
-//     pub fn update_interrupt_enabled(&self) -> Bit {
-//         Bit::new(Register::new(self.base + 0x0C), 0)
-//     }
-
-//     pub fn update_interrupt_flag(&self) -> Bit {
-//         Bit::new(Register::new(self.base + 0x10), 0)
-//     }
-
-//     pub fn update_generator(&self) -> Bit {
-//         Bit::new(Register::new(self.base + 0x14), 0)
-//     }
-
-//     pub fn counter(&self) -> Register16 {
-//         Register16::new(self.base + 0x24)
-//     }
-
-//     pub fn prescaler(&self) -> Register16 {
-//         Register16::new(self.base + 0x28)
-//     }
-
-//     pub fn auto_reload_register(&self) -> Register16 {
-//         Register16::new(self.base + 0x2C)
-//     }
-// }
